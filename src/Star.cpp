8ad17e5a34e7f99617b1d701e7d5c0f30e6676a1
Luomu <kimmo@paahdin.com>
2011-04-27 16:18:59 +0200

    Removed spikes and glow from stars

--- src/Star.cpp
+++ src/Star.cpp
@@ -74,7 +74,7 @@ void Star::Render(const vector3d &viewCoords, const matrix4x4d &viewTransform)
 		const float glowrad = (float)(20.0f+20000.0f*radius/viewCoords.Length());
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE);	
 		glEnable(GL_BLEND);
-		glBegin(GL_TRIANGLE_FAN);
+		/*glBegin(GL_TRIANGLE_FAN);
 		glColor4f(0.5f*col[0], 0.5f*col[1], 0.5f*col[2], 1);
 		glVertex3f(pp.x, pp.y, 0);
 		glColor4f(0,0,0,0);
@@ -83,18 +83,17 @@ void Star::Render(const vector3d &viewCoords, const matrix4x4d &viewTransform)
 		}
 		glVertex3f(pp.x, pp.y+glowrad, 0);
 		glEnd();
-		glDisable(GL_BLEND);
+		glDisable(GL_BLEND);*/
 		
 		Render::State::UseProgram(Render::simpleShader);
 		glEnable(GL_BLEND);
 		glColor4f(0.5f*b*col[0],0.5f*b*col[1],0.5f*b*col[2],1);
-		glBegin(GL_TRIANGLE_FAN);
+		/*glBegin(GL_TRIANGLE_FAN);
 		glVertex3f(pp.x,pp.y,0);
 		glColor4f(0,0,0,0);
 		
 		const float spikerad = std::min<float>(10.0f+20000.0f*radius/viewCoords.Length(), 0.5f*(float)Gui::Screen::GetHeight());
 		{
-			/* cubic bezier with 2 (0,0,0) control points */
 			vector3f p0(0,spikerad,0), p1(spikerad,0,0);
 			float t=0.1; for (int i=1; i<10; i++, t+= 0.1f) {
 				vector3f p = pp + (1-t)*(1-t)*(1-t)*(1-t)*p0 + t*t*t*t*p1;
@@ -122,7 +121,7 @@ void Star::Render(const vector3d &viewCoords, const matrix4x4d &viewTransform)
 				glVertex3fv(&p[0]);
 			}
 		}
-		glEnd();
+		glEnd();*/
 		
 		Gui::Screen::LeaveOrtho();
 		glDisable(GL_BLEND);

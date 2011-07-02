df6581521973dbc59e65e27ae7d5f6a96fa4fb56
kko <pioneer@paahdin.com>
2011-07-02 20:06:48 +0300

    Basic mission structure working

--- data/modules/DeployProbe.lua
+++ data/modules/DeployProbe.lua
@@ -22,7 +22,7 @@ local onChat = function(form, ref, option)
 
 	if option == 0 then --greeting
 		form:SetFace({ female = ad.isfemale, seed = ad.faceseed })
-		form:SetMessage("Hello. There.")
+		form:SetMessage("Hello. There.\n\nYou will need one ton of free cargo space.")
 	elseif option == 1 then --accept
 		form:RemoveAdvertOnClose()
 
@@ -31,7 +31,8 @@ local onChat = function(form, ref, option)
 		local mission = {
 			type     = "Probing",
 			client   = ad.client,
-			location = ad.location,
+			location = ad.targetLocation,
+			clientLocation = ad.location,
 			reward   = ad.reward,
 			due      = ad.due,
 			flavour  = ad.flavour
@@ -41,13 +42,19 @@ local onChat = function(form, ref, option)
 		missions[mref] = mission
 
 		form:SetMessage("Good. Get to it. Must be done by " .. Format.Date(ad.due))
+		Game.player:AddEquip("MINING_MACHINERY")
 		form:AddOption("Hang up.", -1)
 
 		return
 	end
 
-	form:AddOption("I'll do it.", 1)
-	form:AddOption("Hang up.", -1)
+	local stats = Game.player:GetStats()
+	if stats.freeCapacity > 0 then
+		form:AddOption("I'll do it.", 1)
+		form:AddOption("Hang up.", -1)
+	else
+		form:AddOption("I don't have cargo space right now.", -1)
+	end
 end
 
 local onDelete = function(ref)
@@ -69,14 +76,15 @@ local makeAdvert = function(station)
 	local flavour = "WANTED. Someone to probe a ball of rock. Big rewards."
 	local isfemale = Engine.rand:Integer(1) == 1
 	local client = NameGen.FullName(isfemale)
-	local due = Game.time + 50
+	local due = Game.time + 3*24*60*60
 	local reward = 5000
 
 	local ad = {
 		station  = station,
 		flavour  = flavour,
 		client   = client,
-		location = location,
+		location = station.path,
+		targetLocation = location,
 		due      = due,
 		reward   = reward,
 		isfemale = isfemale,
@@ -98,30 +106,74 @@ local onUpdateBB = function(station)
 	--create new missions
 end
 
+local removeMission = function(ref)
+	Game.player:RemoveMission(ref)
+	missions[ref] = nil
+end
+
 local onShipDocked = function(ship, station)
-	if not ship:isPlayer() then return end
-	print("Docked")
+	if not ship:IsPlayer() then return end
 	--check for success, or failure
 	--failure conditions:
 	--time limit exceeded
 	--probe misplaced
+	for ref,mission in pairs(missions) do
+		if mission.location == station.path then
+			if mission.status == 'COMPLETED' then
+				UI.ImportantMessage("That was excellent probing.", mission.client)
+				Game.player:AddMoney(mission.reward)
+				removeMission(ref)
+			elseif mission.status == 'FAILED' or Game.time > mission.due then
+				UI.ImportantMessage("You failure. No money for you.", mission.client)
+				removeMission(ref)
+			end
+		end
+	end
 end
 
 -- Jettisoning on surface = deploy
+-- jettisoning in space is not instant fail as cargo is recoverable
 local onDeploy = function(ship, cargo)
 	if not ship:IsPlayer() then return end
-	if cargo.type ~= 'HYDROGEN' then return end
+	if cargo.type ~= 'MINING_MACHINERY' then return end
 	print(ship.label .. " dumped some " .. cargo.type)
+	for ref,mission in pairs(missions) do
+		-- Correct location?
+		mission.status = 'COMPLETED'
+		mission.location = mission.clientLocation
+		Game.player:UpdateMission(ref, mission)
+	end
+end
+
+local loaded_data
+
+local onGameStart = function ()
+	ads = {}
+	missions = {}
+
+	if not loaded_data then return end
+
+	for k,ad in pairs(loaded_data.ads) do
+		local ref = ad.station:AddAdvert(ad.desc, onChat, onDelete)
+		ads[ref] = ad
+	end
+	for k,mission in pairs(loaded_data.missions) do
+		local mref = Game.player:AddMission(mission)
+		missions[mref] = mission
+	end
+
+	loaded_data = nil
 end
 
 local serialize = function()
-	--save missions status
+	return { ads = ads, missions = missions }
 end
 
 local unserialize = function(data)
-	--load mission status
+	loaded_data = data
 end
 
+EventQueue.onGameStart:Connect(onGameStart)
 EventQueue.onCreateBB:Connect(onCreateBB)
 EventQueue.onUpdateBB:Connect(onUpdateBB)
 EventQueue.onShipDocked:Connect(onShipDocked)

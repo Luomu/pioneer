0d1628d089f65ee107cdb93e90052e6ad2baf639
kko <pioneer@paahdin.com>
2011-11-20 12:13:17 -0500

    Post processing pass

---
+++ src/render/RenderPostPass.h
@@ -0,0 +1,13 @@
+#ifndef _RENDERPOSTPASS_H
+#define _RENDERPOSTPASS_H
+
+namespace Render {
+	namespace Post {
+		/* Post processing pass, runs a (filter) operation or several */
+		class Pass {
+
+		};
+	}
+}
+
+#endif

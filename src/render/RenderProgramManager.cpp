ad082ea4e1a461b31daa14498afa8b0e40659e68
kko <pioneer@paahdin.com>
2011-12-10 15:15:53 -0500

    Forgot to cache the program in manager

--- src/render/RenderProgramManager.cpp
+++ src/render/RenderProgramManager.cpp
@@ -8,10 +8,15 @@ RefCountedPtr<Post::Program> ProgramManager::Request(const std::string &vtxName,
 	using std::string;
 	using std::pair;
 	pair<string, string> p = std::make_pair(vtxName, fragName);
+
+	// check for existing
 	std::map<pair<string, string>, RefCountedPtr<Post::Program> >::iterator i = m_programs.find(p);
 	if (i != m_programs.end())
 		return (*i).second;
+
+	// create new
 	RefCountedPtr<Post::Program> prog(new Post::Program(vtxName, fragName));
+	m_programs.insert(std::make_pair(p, prog));
 	return prog;
 }
 

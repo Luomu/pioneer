ffbdb82c686498b278925170da773e83318d207f
kko <pioneer@paahdin.com>
2013-04-20 16:30:49 -0400

    Disable random factions for faster startup

--- data/factions/00_AutoGenerate.lua
+++ data/factions/00_AutoGenerate.lua
@@ -1,3 +1,4 @@
+--[[
 -- Copyright © 2008-2013 Pioneer Developers. See AUTHORS.txt for details
 -- Licensed under the terms of the GPL v3. See licenses/GPL-3.txt
 
@@ -174,3 +175,4 @@ end
 
 ------------------------------------------------------------------------------
 -- END generation
+]]--
